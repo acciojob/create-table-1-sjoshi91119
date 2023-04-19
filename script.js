c686a2e5ce91a57440a6e0d8fb9674091c3f4424
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+
+	let str = document.querySelector("#sampleTable>tbody").innerHTML;
+
+    let nstr = `<tr>
+                 <td>New Cell1</td>
+                 <td>New Cell2</td>
+                 </tr>`
+
+    document.querySelector("#sampleTable>tbody").innerHTML = nstr + str;
   
 }
